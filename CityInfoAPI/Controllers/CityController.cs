74e4ea3b0c0d9d469910d737edc21175c3cc3c3f
Omar Zaarour <omar.zaarour@adoxio.com>
2018-01-21 19:36:50 -0700

    Add project files.

---
+++ CityInfoAPI/Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CityController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new {Id=1,Name="New york City" },
+                new {Id=2,Name="LA City"}
+            });
+        }
+    }
+}
